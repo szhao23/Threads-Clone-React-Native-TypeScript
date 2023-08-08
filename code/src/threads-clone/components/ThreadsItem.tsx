11ca8c5023c5bdcf68f7692b21a0e942dab767c9
szhao23 <stevenzhao456@gmai.com>
2023-08-08 00:16:25 -0500

    Added Bottom Icons

--- code/src/threads-clone/components/ThreadsItem.tsx
+++ code/src/threads-clone/components/ThreadsItem.tsx
@@ -1,6 +1,6 @@
 import { Thread } from "../types/threads";
 import * as React from "react";
-import { View } from "react-native";
+import { View, useColorScheme } from "react-native";
 import { Text } from "./Themed";
 import { Ionicons, Feather, AntDesign, FontAwesome } from "@expo/vector-icons";
 import { timeAgo } from "../utils/time-ago";
@@ -16,6 +16,7 @@ export default function ThreadsItem(thread: Thread): JSX.Element {
           createdAt={thread.createdAt}
           verified={thread.author.verified}
         />
+        <BottomIcons />
         <PostFooter replies={thread.repliesCount} likes={thread.likesCount} />
       </View>
     </View>
@@ -61,5 +62,20 @@ function PostFooter({ replies, likes }: { replies: number; likes: number }) {
     <Text style={{ color: "gray" }}>
       {replies} replies · {likes} likes
     </Text>
+  );
+}
+
+// Icons
+function BottomIcons() {
+  const iconSize = 20;
+  const currentTheme = useColorScheme();
+  const iconColor = currentTheme === "dark" ? "white" : "black";
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+      <FontAwesome name="heart-o" size={iconSize} color={iconColor} />
+      <Ionicons name="chatbubble-outline" size={iconSize} color={iconColor} />
+      <AntDesign name="retweet" size={iconSize} color={iconColor} />
+      <Feather name="send" size={iconSize} color={iconColor} />
+    </View>
   );
 }
